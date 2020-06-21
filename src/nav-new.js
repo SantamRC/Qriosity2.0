a681d2e1cae002aadefa1814659bfdc66229d356
SantamRC <roychoudhurysantam00@gmail.com>
2020-06-22 03:28:07 +0530

    add

--- src/nav-new.js
+++ src/nav-new.js
@@ -8,14 +8,14 @@ export default function NAVN(){
             <div className='navbar'>
                 <Route>             
                     <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-                        <Link to='/'><a class="navbar-brand" >Qriosity2.0</a></Link>
+                        <Link to='/'><div class="navbar-brand" >Qriosity2.0</div></Link>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                                 <ul class="navbar-nav">
                                 <li class="nav-item">
-                                   <Link to='/login'><a class="nav-link" >LOGIN</a></Link>
+                                   <Link to='/login'><div class="nav-link" >LOGIN</div></Link>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">RULES</a>
