4a4f5e16386b347d7af5b7c0dc7ee1c564ea7298
SantamRC <roychoudhurysantam00@gmail.com>
2020-06-22 02:52:52 +0530

    nav

--- src/nav-new.js
+++ src/nav-new.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './CSS/nav-new.css'
-//import './CSS/nav.css'
-//import {Link, Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default function NAVN(){
     return(
@@ -12,19 +12,21 @@ export default function NAVN(){
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                            <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">LOGIN</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">RULES</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">ABOUT</a>
-                            </li>    
-                            </ul>
-                        </div>  
+                        <Route>
+                            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                                <ul class="navbar-nav">
+                                <li class="nav-item">
+                                   <Link to='/login'><a class="nav-link" href="#">LOGIN</a></Link> 
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">RULES</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ABOUT</a>
+                                </li>    
+                                </ul>
+                            </div>
+                        </Route>  
                     </nav>
                 
                 
