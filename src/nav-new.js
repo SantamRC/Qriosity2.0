2775b26d0ba2c2dd9b5a542136e2633edc4c082d
SantamRC <roychoudhurysantam00@gmail.com>
2020-06-22 03:22:52 +0530

    new

--- src/nav-new.js
+++ src/nav-new.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './CSS/nav-new.css'
+import { Route,Link} from 'react-router-dom'
 
 export default function NAVN(){
     return(
         <div>
-            <div className='navbar'>               
+            <div className='navbar'>
+                <Route>             
                     <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-                        <a class="navbar-brand" href="/">Qriosity2.0</a>
+                        <Link to='/'><a class="navbar-brand" >Qriosity2.0</a></Link>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                                 <ul class="navbar-nav">
                                 <li class="nav-item">
-                                   <a class="nav-link" href="/login">LOGIN</a>
+                                   <Link to='/login'><a class="nav-link" >LOGIN</a></Link>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">RULES</a>
@@ -23,9 +25,8 @@ export default function NAVN(){
                                 </li>    
                                 </ul>
                             </div>
-                    </nav>
-                
-                
+                    </nav>               
+                </Route>  
             </div>
         </div>
     )
